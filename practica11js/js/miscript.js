3148af64b662d7887e0a8bf815e91ad8b19536c7
Jesus Javier Verdugo Reyes <143364191+javierVerdugoR@users.noreply.github.com>
2024-05-27 10:27:21 -0700

    practicas css y html terminadas

---
+++ practica11js/js/miscript.js
@@ -0,0 +1,12 @@
+
+var a=0;
+let b=0;
+const pi=3.141617
+console.log("hola mundo")
+alert("javascript en proceso");
+function boton(){
+    alert("Clic");
+}
+const fechas=()=>{
+    alert("funcion flecha-anonima")
+}
